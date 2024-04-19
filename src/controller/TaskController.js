10ac3ad4b6a49e05dca8ffaff3def90129a0fe72
HpSharma <harpalhmh@gmail.com>
2024-04-18 21:26:14 -0500

    implement cache in tasks controller

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,10 +1,17 @@
 const Task = require('../models/task');
 const validator = require('validator');
+const cache = require('../../cache');
 
 
 exports.getAllTasks = async (req, res) => {
   try {
-    const tasks = await Task.find();
+    let tasks = [];
+    if (cache.has("tasks")) {
+      tasks = cache.get("tasks");
+    } else {
+      tasks = await Task.find();
+      cache.set('tasks', tasks);
+    }
     res.render('home', { tasks });
   } catch (err) {
     console.error(err);
@@ -20,6 +27,7 @@ exports.addTask = async (req, res) => {
   try {
     const task = new Task({ title, description });
     await task.save();
+    cache.del('tasks');
     res.redirect('/');
   } catch (err) {
     console.error(err);
@@ -31,6 +39,7 @@ exports.deleteTask = async (req, res) => {
   const { id } = req.params;
   try {
     await Task.findByIdAndDelete(id);
+    cache.del('tasks');
     res.redirect('/');
   } catch (err) {
     console.error(err);
@@ -49,6 +58,7 @@ exports.updateTask = async (req, res) => {
     task.description = description;
     task.completed = completed ? true : false;
     await task.save();
+    cache.del('tasks');
     res.redirect('/');
   } catch (err) {
     console.error(err);
